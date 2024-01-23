b248f9f0259bfce18b4a2a02272406d0efe66f71
Landofein <landenfinlinson@yahoo.com>
2024-01-23 13:28:01 -0500

    Finished HW1 Added NistClient

---
+++ src/main/java/ClientNist.java
@@ -0,0 +1,10 @@
+public class ClientNist {
+    public static void main(String[] args) throws Exception {
+        //expect two command-line arguments
+        if (args.length != 1) {
+            System.out.println("Syntax: Client <serverIP> <serverPort>");
+            return;
+        }
+
+    }
+}
