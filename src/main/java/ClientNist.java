4fe804491391bbc6ed2b4321d85f39271340f110
Landofein <landenfinlinson@yahoo.com>
2024-01-24 14:46:49 -0500

    Added Project2 Changes

--- src/main/java/ClientNist.java
+++ src/main/java/ClientNist.java
@@ -1,10 +1,51 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ClientNist {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         //expect two command-line arguments
-        if (args.length != 1) {
+        if(args.length != 2){
             System.out.println("Syntax: Client <serverIP> <serverPort>");
             return;
         }
 
+        InetAddress serverIP = InetAddress.getByName(args[0]);
+        int serverPort = Integer.parseInt(args[1]);
+
+        //"GetTime", "GetDate"
+        Scanner keyboard = new Scanner(System.in);
+        String message = keyboard.nextLine();
+
+        DatagramPacket request = new DatagramPacket(
+                message.getBytes(),
+                message.getBytes().length
+                ,serverIP, serverPort);
+
+        //allows the OS to:
+        // - allocate memory buffer
+        // - assign a random port number to this socket
+
+        DatagramSocket socket = new DatagramSocket();
+
+        socket.send(request);
+
+        DatagramPacket clientRequest = new DatagramPacket(
+                new byte[128],
+                128);
+
+        socket.receive(clientRequest);
+
+        byte[] content = Arrays.copyOf(
+                clientRequest.getData(),
+                clientRequest.getLength());
+
+        String serverMessage = new String(content);
+
+        System.out.println(serverMessage);
+
     }
+
 }
