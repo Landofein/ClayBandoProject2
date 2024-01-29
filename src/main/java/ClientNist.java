835361a8c172129b69e5180a256dce9fffc7d3c9
Landofein <landenfinlinson@yahoo.com>
2024-01-29 16:49:42 -0500

    Finished Project2

--- src/main/java/ClientNist.java
+++ src/main/java/ClientNist.java
@@ -1,51 +1,41 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.nio.ByteBuffer;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class ClientNist {
-    public static void main(String[] args) throws Exception{
-        //expect two command-line arguments
-        if(args.length != 2){
-            System.out.println("Syntax: Client <serverIP> <serverPort>");
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Syntax: TimeClient <serverIP> <serverPort>");
             return;
         }
 
         InetAddress serverIP = InetAddress.getByName(args[0]);
         int serverPort = Integer.parseInt(args[1]);
 
-        //"GetTime", "GetDate"
-        Scanner keyboard = new Scanner(System.in);
-        String message = keyboard.nextLine();
-
-        DatagramPacket request = new DatagramPacket(
-                message.getBytes(),
-                message.getBytes().length
-                ,serverIP, serverPort);
-
-        //allows the OS to:
-        // - allocate memory buffer
-        // - assign a random port number to this socket
 
         DatagramSocket socket = new DatagramSocket();
-
+        DatagramPacket request = new DatagramPacket(new byte[0], 0, serverIP, serverPort);
         socket.send(request);
 
-        DatagramPacket clientRequest = new DatagramPacket(
-                new byte[128],
-                128);
 
-        socket.receive(clientRequest);
+        DatagramPacket response = new DatagramPacket(new byte[4], 4);
+        socket.receive(response);
 
-        byte[] content = Arrays.copyOf(
-                clientRequest.getData(),
-                clientRequest.getLength());
 
-        String serverMessage = new String(content);
+        long secondsSince1900 = ByteBuffer.wrap(response.getData()).getInt() & 0xFFFFFFFFL;
 
-        System.out.println(serverMessage);
 
+        ZonedDateTime dateTime = Instant.ofEpochSecond(secondsSince1900 - 2208988800L)
+                .atZone(ZoneId.of("America/New_York"));
+
+
+        System.out.println("Current Date & Time (Eastern Time Zone): " + dateTime);
+
+
+        socket.close();
     }
-
 }
